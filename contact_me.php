93f834c01539df6f7552584687ac0bcbcef2b0c6
Subhajit <sarkar.subhajit22@gmail.com>
2019-11-27 19:35:32 +0530

    bug fixes for php mail

--- contact_me.php
+++ contact_me.php
@@ -1,15 +1,10 @@
 <?php
 
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-
     $fname = $_POST['fName'];
     $lname = $_POST['lName'];
     $email = $_POST['emailId'];
     $phone = $_POST['contact_no'];
     $message = $_POST['message'];
-    $planDetail = array();
-    $serviceDetail = array();
 
     header('Content-Type: application/json');
 
@@ -43,27 +38,14 @@
         exit();
     }
 
-    if(!empty($_POST['plans'])) {
-        foreach($_POST['plans'] as $plan) {
-            array_push($planDetail, $plan);
-        }
-    }
-
-    if(!empty($_POST['services'])) {
-        foreach($_POST['services'] as $service) {
-            array_push($serviceDetail, $service);
-        }
-    }
-
     $name = $fname . $lname;
 
     // Create the email and send the message
-    $from = &$email;
     $to = "care.surfowl@gmail.com"; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.    
     $subject = "Website Contact Form:  $name";
-    $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nE-Mail ID: $email\n\nPhone: $phone\n\nMessage:\n$message\n\nPlans: \n$planDetail\n\nServices: \n$serviceDetail";
+    $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nE-Mail ID: $email\n\nPhone: $phone\n\nMessage:\n$message";
     $headers = "From: noreply@surfowl.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-    $headers .= "Reply-To: $email_address" . $from;   
+    $headers = "Reply-To: " . $email;   
     mail($to,$subject,$body,$headers);
     return true;        
 ?>
